c099321f618ce2c0c4aa5c9dc08d332124e6dbd5
Serge Poltavski <serge.poltavski@gmail.com>
2015-02-18 01:51:28 +0400

    build script added

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+LILY="lilypond -dno-point-and-click --pdf "
+CWD=`pwd`
+
+find scores -name *.ly -print0 | while read -d $'\0' file
+do
+	f=`echo $file | grep -v 'src\,'`
+	if [ -n "$f" ] 
+	then
+		composer=`cat "$file" | sed -n 's/^info-composer[[:blank:]]*=[[:blank:]]*"\([^"]*\)"/\1/p'`
+		if [ -z "$composer" ]; then continue; fi
+		title=`cat "$file" | sed -n 's/^info-title[[:blank:]]*=[[:blank:]]*"\([^"]*\)"/\1/p'`
+		
+		in_file=`basename "$file"`
+		dir=`dirname "$file"`
+
+		out_fname=`echo "$composer-$title-$in_file" | sed s'/ /_/g'`
+		out_fname="${out_fname%.ly}"
+		out_fname_ext="${out_fname}.pdf"
+
+		cd "$dir"
+
+		echo "Output to $out_fname_ext"
+		${LILY} -o "$out_fname" "$in_file" 1>/dev/null
+		mv "$out_fname_ext" "../../pdf/$out_fname_ext"
+		cd "$CWD"
+	fi
+done
+
+cd "$CWD"
+
